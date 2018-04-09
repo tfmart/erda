c6c0773cc3583431ef610be4b9fcd09a45962ded
tfmart <tomfmart@gmail.com>
2018-04-09 15:28:33 -0300

    Biblioteca Matrizes
    Arquivos para a biblioteca Matrizes.h que será utilizado no projeto

---
+++ Projeto1/matrizes.c
@@ -0,0 +1,50 @@
+#define <stdio.h>
+#define <string.h>
+#define "matrizes.h"
+
+struct matrizes {
+    char nome;
+    int numero;
+    matrizes *prox;
+}
+
+Matriz declaracao {
+    
+}
+
+Matriz destruir {
+    
+}
+
+void imprimir {
+    
+}
+
+void atribuirElemento {
+    
+}
+
+void atribuirLinha{
+    
+}
+
+void transporMatriz{
+    
+}
+
+void soma{
+    
+}
+
+void divisao{
+    
+}
+
+void multMatriz1{
+    
+}
+
+void multMatriz2{
+    
+}
+
---
+++ Projeto1/matrizes.h
@@ -0,0 +1,51 @@
+#ifndef MATRIZES_H_
+#define MATRIZES_H_
+
+typedef struct matriz Matriz;
+
+Matriz declaracao;
+//Permite a criação de uma matriz
+//ENTRADA: CM <nome sem espa os> <n mero de linhas> <n mero de colunas>
+//SAIDA: OK ou ERRO (caso matriz ja exista ou uma das dimensoes seja menor do que 1 ou maior que 50)
+
+Matriz destruir;
+//Remove uma matriz
+//ENTDADA: DM <nome>
+//SAIDA: OK ou ERRO
+
+void imprimir;
+//Imprime uma matriz
+//ENTRADA: IM <nome>
+//SAIDA: a(1,1) ... a(1, m) (SEPARADOS POR ESPAÇO)
+//       a(n,1) ... a(n, m)
+//       ou ERRO caso a matriz nao exista
+
+void atribuirElemento;
+//Atribui um numero em uma posicao de um vetor
+//ENTRADA: AE <nome> <linha> <coluna> <valor>
+//SAIDA: OK ou ERRO (caso o vetor nao exista ou a posicao seja invalida)
+
+void atribuirLinha;
+//Atribui uma linha de elementos a uma coluna de um vetor
+//ENTRADA: AL <nome> <linha> <val1> ... <valn>
+//SAIDA: OK ou ERRO (caso o vetor nao exista, a coluna seja invalida ou a qtde de elementos nao seja condizente com as dimensoes do vetor)
+
+void transporMatriz;
+//Transpoe uma matriz
+//ENTRADA: TM <nome> <nome-resultado sem espacos>
+//SAIDA: Imprime a matriz resultante ou ERRO (caso alguma das matrizes nao exista, suas dimensoes nao sejam as mesmas ou a matriz resultante ja existe)
+
+void soma;
+//Realiza a soma de duas matrizes
+//ENTRADA: SM <nome1> <nome2> <nome-resultado sem espacos>
+//SAIDA: mprime a matriz resultante ou ERRO (caso alguma das matrizes nao exista, suas dimensoes nao sejam as mesmas ou a matriz resultante ja existe)
+
+void divisao;
+//Divide uma matriz pela outra (elemento a elemento)
+//ENTRADA: DV <nome1> <nome2> <nome-resultado sem espacos>
+//SAIDA: OK ou ERRO (caso o vetor nao exista ou a posicao seja invalida)
+
+void multMatriz1;
+void multMatriz2;
+
+#endif
