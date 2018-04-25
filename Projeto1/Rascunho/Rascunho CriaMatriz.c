08a16611007e8764b6331cee2be9419c563c172f
tfmart <tomfmart@gmail.com>
2018-04-25 20:31:20 -0300

    Funções de Destruir Matriz e Pesquisa
    Agora é possível apagar uma matriz previamente criada e buscar por uma matriz, através de seu nome.

--- Projeto1/Rascunho/Rascunho CriaMatriz.c
+++ Projeto1/Rascunho/Rascunho CriaMatriz.c
@@ -1,3 +1,5 @@
+//OBS: O programa não verifica se o nome dado para matriz a ser criada já foi utilizado anteriormente.
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,104 +13,145 @@ struct No {
 
 typedef struct No celula;
 
-int criarMatriz(celula **lista, int linhas, int colunas, char nome[]) {
+int buscaMatriz(celula **lista, char nome[]) {
     celula *aux = *lista;
-    int contador;
+    int c1, c2;
     
-    //Verifica as condições para que o programa não retorne erro
-    if((linhas < 1 || linhas > 50) || (colunas < 1 || colunas > 50)) return 0;
-    if (aux != NULL) {
-        aux = *lista;
-        while (aux->prox == NULL) {
-            if ((strcmp(aux->nomeMatriz, nome)) == 0) {
-                return 0;
-                break;
-            }
-            aux = aux->prox;
-        }
-    }
-    aux = *lista;
-    
-    //caso a lista esteja vazia (registra no comeco da lista)
-    if (aux == NULL) {
-        aux = (celula*)malloc(sizeof(celula));
-        aux->linhas = linhas;
-        aux->colunas = colunas;
-        strcpy(aux->nomeMatriz, nome);
-        
-        //cria matriz
-        aux->matriz = (float**)malloc(linhas * sizeof(sizeof(float*)));
-        for (contador = 0; contador < colunas; contador++) {
-            aux->matriz[contador] = (float*)malloc(colunas * sizeof(float));
-        }
-        
-//         //PREENCHENDO MATRIZ P/ TESTES
-//         for (int i = 0; i < colunas; i++) {
-//             for (int j = 0; j < linhas; j++) {
-//                 aux->matriz[i][j] = 2;
-//             }
-//         }
-        aux->prox = *lista;
-        *lista = aux;
-    }
-    
-    //caso a lista ja tenha pelo menos um nó (registra no final da lista)
+    if(aux == NULL) return 0;
     else {
-        aux = *lista;
-        celula *aux2 = (celula*)malloc(sizeof(celula));
-        aux2 = (celula*)malloc(sizeof(celula));
-        aux2->linhas = linhas;
-        aux2->colunas = colunas;
-        strcpy(aux2->nomeMatriz, nome);
-        
-        //cria matriz
-        aux->matriz = (float**)malloc(linhas * sizeof(sizeof(float*)));
-        for (contador = 0; contador < colunas; contador++) {
-            aux->matriz[contador] = (float*)malloc(colunas * sizeof(float));
-        }
-        
-        while (aux->prox) {
+        while (aux->prox != NULL) {
+            if (strcmp(aux->nomeMatriz, nome) == 0) {
+                for (c1 = 0; c1 < aux->linhas; c1++) {
+                    for (c2 = 0; c2 < aux->colunas; c2++) {
+                        printf("%6.2f ", aux->matriz[c1][c2]);
+                    }
+                    printf("\n");
+                }
+                return 1;
+            }
             aux = aux->prox;
         }
-        aux->prox = aux2;
-        
-        //PREENCHENDO MATRIZ P/ TESTES
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas; j++) {
-                aux->matriz[i][j] = 2.0;
+        //condicao p/ ultimo elemento da lista
+        if (strcmp(aux->nomeMatriz, nome) == 0) {
+            for (c1 = 0; c1 < aux->linhas; c1++) {
+                for (c2 = 0; c2 < aux->colunas; c2++) {
+                    printf("%6.2f ", aux->matriz[c1][c2]);
+                }
+                printf("\n");
             }
+            return 1;
         }
-        
-        
     }
+    return 0;
+}
+
+int criarMatriz(celula **lista, int linhas, int colunas, char nome[]) {
+    celula *aux = (*lista);
+    celula *novo = (celula*)malloc(sizeof(celula));
+    int contador;
+
+    //valida a criação da matriz
+    if((linhas < 1 || linhas > 50) || (colunas < 1 || colunas > 50)) return 0;
+
+    strcpy(novo->nomeMatriz, nome);
+    novo->linhas = linhas;
+    novo->colunas = colunas;
+    novo->matriz = (float**)malloc(linhas * sizeof(sizeof(float*)));
+    for (contador = 0; contador < colunas; contador++) {
+        novo->matriz[contador] = (float*)malloc(colunas * sizeof(float));
+    }
+    novo->prox = NULL;
     
+    if (aux == NULL) {
+        *lista = novo;
+    } else {
+        while (aux->prox != NULL) {
+            aux = aux->prox;
+        }
+        aux->prox = novo;
+    }
+
+
     return 1;
 }
 
-
+int destruirMatriz(celula **lista, char nome[]) {
+    celula *aux = *lista;
+    celula *temp = *lista;
+    //condicao para primeiro elemento da lista
+    if (aux == NULL) return 0;
+    if (strcmp(aux->nomeMatriz, nome) == 0) {
+        temp = temp->prox;
+        free(aux);
+        *lista = temp;
+        return 1;
+    } else {                                            //outros elementos da lista
+        while (aux->prox != NULL) {
+            if (strcmp(aux->nomeMatriz, nome) == 0) {
+                temp->prox = aux->prox;
+                free(aux);
+                return 1;
+            }
+            temp = aux;
+            aux = aux->prox;
+        }
+    }
+    //condicao para ultimo elemento da lista
+    if (strcmp(aux->nomeMatriz, nome) == 0) {
+        temp->prox = aux->prox;
+        free(aux);
+        return 1;
+    }
+    return 0;
+}
 
 int main() {
-    int linhas, colunas;
+    int linhas, colunas, menu = 0;
     char nome[20];
     celula *lista;
-    printf("digite um nome para a sua matriz: ");
-    fgets(nome, sizeof(nome), stdin);
-    strtok(nome, "\n");
-    printf("Digite a quantidade de linhas e colunas: ");
-    scanf("%i %i", &linhas, &colunas);
-    if (criarMatriz(&lista, linhas, colunas, nome) != 1) printf("ERRO\n");
-    else {
-        printf("OK\n");
-        //IMPRIME CELULA PARA TESTE
-//        printf("Nome da celula: %s\n", lista->nomeMatriz);
-//        printf("Linhas %i, colunas %i\n", lista->linhas, lista->colunas);
-//        for (int i = 0; i < colunas; i++) {
-//            for (int j = 0; j < linhas; j++) {
-//                printf("%.1f ", lista->matriz[i][j]);
-//            }
-//            printf("\n");
-//        }
-    }
     
+    do {
+        printf("1- Cadastrar matriz\n");
+        printf("2- Remover matriz\n");
+        printf("3- Buscar uma matriz\n");
+        printf("4- Finalizar programa\n");
+        scanf("%i", &menu);
+        switch (menu) {
+            case 1:
+                fpurge(stdin);
+                printf("digite um nome para a sua matriz: ");
+                fgets(nome, sizeof(nome), stdin);
+                strtok(nome, "\n");
+                printf("Digite a quantidade de linhas e colunas: ");
+                fpurge(stdin);
+                scanf("%i %i", &linhas, &colunas);
+                if (criarMatriz(&lista, linhas, colunas, nome) != 1) printf("ERRO\n");
+                else printf("OK\n");
+                break;
+            case 2:
+                fpurge(stdin);
+                printf("Digite o nome da matriz a ser removida: ");
+                fgets(nome, sizeof(nome), stdin);
+                strtok(nome, "\n");
+                if(destruirMatriz(&lista, nome) == 1) printf("OK\n");
+                else printf("ERRO\n");
+                break;
+            case 3:
+                fpurge(stdin);
+                printf("Digite o nome da matriz que deseja buscar: ");
+                fgets(nome, sizeof(nome), stdin);
+                strtok(nome, "\n");
+                if (buscaMatriz(&lista, nome) == 0) printf("ERRO\n");
+                break;
+            case 4:
+                printf("Finalizando...\n");
+                break;
+            default:
+                printf("Opção inválida\n");
+                break;
+        }
+        
+    } while (menu != 4);
+
     return 0;
 }
