3cd14e2305537b9b897b4fb9a80d62f7cd9bc78b
tfmart <tomfmart@gmail.com>
2018-04-26 16:24:05 -0300

    Funções 1, 2 e 3
    Inclusão das funções de criação de matrizes, apagar matrizes e de imprimir uma matriz

--- Projeto1/matrizes.c
+++ Projeto1/matrizes.c
@@ -2,22 +2,109 @@
 #define <string.h>
 #define "matrizes.h"
 
-struct matrizes {
-    char nome;
-    int numero;
-    matrizes *prox;
+struct No {
+    char nomeMatriz[20];
+    int linhas, colunas;
+    float **matriz;
+    struct No *prox;
+};
+
+int CM(celula **lista, int linhas, int colunas, char nome[]) {
+    celula *aux = (*lista);
+    celula *aux2 = (*lista);
+    celula *novo = (celula*)malloc(sizeof(celula));
+    int contador;
+    
+    //valida a criação da matriz
+    if((linhas < 1 || linhas > 50) || (colunas < 1 || colunas > 50)) return 0;
+    if (aux2 != NULL) {
+        if (strcmp(aux2->nomeMatriz, nome) == 0) return 0;
+        while (aux->prox != NULL) {
+            if (strcmp(aux2->nomeMatriz, nome) == 0) return 0;
+            aux2 = aux2->prox;
+        }
+    }
+    
+    strcpy(novo->nomeMatriz, nome);
+    novo->linhas = linhas;
+    novo->colunas = colunas;
+    novo->matriz = (float**)malloc(linhas * sizeof(sizeof(float*)));
+    for (contador = 0; contador < colunas; contador++) {
+        novo->matriz[contador] = (float*)malloc(colunas * sizeof(float));
+    }
+    novo->prox = NULL;
+    
+    if (aux == NULL) {
+        *lista = novo;
+    } else {
+        while (aux->prox != NULL) {
+            aux = aux->prox;
+        }
+        aux->prox = novo;
+    }
+    return 1;
 }
 
-Matriz declaracao {
-    
+int DM(celula **lista, char nome[]) {
+    celula *aux = *lista;
+    celula *temp = *lista;
+    //condicao para primeiro elemento da lista
+    if (aux == NULL) return 0;
+    if (strcmp(aux->nomeMatriz, nome) == 0) {
+        temp = temp->prox;
+        free(aux);
+        *lista = temp;
+        return 1;
+    } else {                                            //outros elementos da lista
+        while (aux->prox != NULL) {
+            if (strcmp(aux->nomeMatriz, nome) == 0) {
+                temp->prox = aux->prox;
+                free(aux);
+                return 1;
+            }
+            temp = aux;
+            aux = aux->prox;
+        }
+    }
+    //condicao para ultimo elemento da lista
+    if (strcmp(aux->nomeMatriz, nome) == 0) {
+        temp->prox = aux->prox;
+        free(aux);
+        return 1;
+    }
+    return 0;
 }
 
-Matriz destruir {
+int buscaMatriz(celula **lista, char nome[]) {
+    celula *aux = *lista;
+    int c1, c2;
     
-}
-
-void imprimir {
-    
+    if(aux == NULL) return 0;
+    else {
+        while (aux->prox != NULL) {
+            if (strcmp(aux->nomeMatriz, nome) == 0) {
+                for (c1 = 0; c1 < aux->linhas; c1++) {
+                    for (c2 = 0; c2 < aux->colunas; c2++) {
+                        printf("%6.2f ", aux->matriz[c1][c2]);
+                    }
+                    printf("\n");
+                }
+                return 1;
+            }
+            aux = aux->prox;
+        }
+        //condicao p/ ultimo elemento da lista
+        if (strcmp(aux->nomeMatriz, nome) == 0) {
+            for (c1 = 0; c1 < aux->linhas; c1++) {
+                for (c2 = 0; c2 < aux->colunas; c2++) {
+                    printf("%6.2f ", aux->matriz[c1][c2]);
+                }
+                printf("\n");
+            }
+            return 1;
+        }
+    }
+    return 0;
 }
 
 void atribuirElemento {
