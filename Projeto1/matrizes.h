4e4140c873cf5c0c45afc577d371ffb6ea76d895
tfmart <tomfmart@gmail.com>
2018-05-03 09:47:32 -0300

    Atualização dos nomes das funções

--- Projeto1/matrizes.h
+++ Projeto1/matrizes.h
@@ -3,49 +3,56 @@
 
 typedef struct No celula;
 
-Matriz CM;
+int CM(celula **lista, int linhas, int colunas, char nome[]);
 //Permite a criação de uma matriz
 //ENTRADA: CM <nome sem espa os> <n mero de linhas> <n mero de colunas>
 //SAIDA: OK ou ERRO (caso matriz ja exista ou uma das dimensoes seja menor do que 1 ou maior que 50)
 
-Matriz DM;
+int DM(celula **lista, char nome[]);
 //Remove uma matriz
 //ENTDADA: DM <nome>
 //SAIDA: OK ou ERRO
 
-void IM;
+int IM(celula **lista, char nome[]);
 //Imprime uma matriz
 //ENTRADA: IM <nome>
 //SAIDA: a(1,1) ... a(1, m) (SEPARADOS POR ESPAÇO)
 //       a(n,1) ... a(n, m)
 //       ou ERRO caso a matriz nao exista
 
-void atribuirElemento;
+void AE(void);
 //Atribui um numero em uma posicao de um vetor
 //ENTRADA: AE <nome> <linha> <coluna> <valor>
 //SAIDA: OK ou ERRO (caso o vetor nao exista ou a posicao seja invalida)
 
-void atribuirLinha;
+void AL(void);
 //Atribui uma linha de elementos a uma coluna de um vetor
 //ENTRADA: AL <nome> <linha> <val1> ... <valn>
 //SAIDA: OK ou ERRO (caso o vetor nao exista, a coluna seja invalida ou a qtde de elementos nao seja condizente com as dimensoes do vetor)
 
-void transporMatriz;
+void AC(void);
+//Atribui uma coluna de elementos a um vetor
+
+int TM(celula **lista,char nome, char nomeResultado);
 //Transpoe uma matriz
 //ENTRADA: TM <nome> <nome-resultado sem espacos>
 //SAIDA: Imprime a matriz resultante ou ERRO (caso alguma das matrizes nao exista, suas dimensoes nao sejam as mesmas ou a matriz resultante ja existe)
 
-void soma;
+int SM(celula **lista,char nome[],char nome1[],char nomeResultado);
 //Realiza a soma de duas matrizes
 //ENTRADA: SM <nome1> <nome2> <nome-resultado sem espacos>
 //SAIDA: mprime a matriz resultante ou ERRO (caso alguma das matrizes nao exista, suas dimensoes nao sejam as mesmas ou a matriz resultante ja existe)
 
-void divisao;
+int DV(celula **lista,char nome[],char nome1[],char nomeResultado);
 //Divide uma matriz pela outra (elemento a elemento)
 //ENTRADA: DV <nome1> <nome2> <nome-resultado sem espacos>
 //SAIDA: OK ou ERRO (caso o vetor nao exista ou a posicao seja invalida)
 
-void multMatriz1;
-void multMatriz2;
+void MM(void);
+
+int ME(celula **lista,char nome[],char nome1[],char nomeResultado);
+
+void FE(void) 
+//finaliza o programa
 
 #endif
