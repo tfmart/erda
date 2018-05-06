2e177bf9fbce05ca33778bd3979a26f887bf9277
tfmart <tomfmart@gmail.com>
2018-05-06 18:20:51 -0300

    Atualizando nome das funções
    Mudei o nome das funções para ficar de acordo com as mudanças de nome feita pela Milena em matrizes.c

--- Projeto1/matrizes.h
+++ Projeto1/matrizes.h
@@ -3,54 +3,54 @@
 
 typedef struct No celula;
 
-int CM(int linhas, int colunas, char nome[]);
+int criarMatriz(int linhas, int colunas, char nome[]);
 //Permite a criação de uma matriz
 //ENTRADA: CM <nome sem espa os> <n mero de linhas> <n mero de colunas>
 //SAIDA: OK ou ERRO (caso matriz ja exista ou uma das dimensoes seja menor do que 1 ou maior que 50)
 
-int DM(char nome[]);
+int destruirMatriz(char nome[]);
 //Remove uma matriz
 //ENTDADA: DM <nome>
 //SAIDA: OK ou ERRO
 
-int IM(char nome[]);
+int imprimirMatriz(char nome[]);
 //Imprime uma matriz
 //ENTRADA: IM <nome>
 //SAIDA: a(1,1) ... a(1, m) (SEPARADOS POR ESPAÇO)
 //       a(n,1) ... a(n, m)
 //       ou ERRO caso a matriz nao exista
 
-int AE(char nome[], int linha, int coluna, float n);
+int atribuirElemento(char nome[], int linha, int coluna, float n);
 //Atribui um numero em uma posicao de um vetor
 //ENTRADA: AE <nome> <linha> <coluna> <valor>
 //SAIDA: OK ou ERRO (caso o vetor nao exista ou a posicao seja invalida)
 
-void AL(celula **Lista, int linhas, char Nome[]);
+void atribuirLinha(celula **Lista, int linhas, char Nome[]);
 //Atribui uma linha de elementos a uma coluna de um vetor
 //ENTRADA: AL <nome> <linha> <val1> ... <valn>
 //SAIDA: OK ou ERRO (caso o vetor nao exista, a coluna seja invalida ou a qtde de elementos nao seja condizente com as dimensoes do vetor)
 
-void AC(celula **Lista, int colunas, char Nome[]);
+void atribuirColuna(celula **Lista, int colunas, char Nome[]);
 //Atribui uma coluna de elementos a um vetor
 
-int TM(char nome, char nomeResultado[]);
+int transporMatriz(char nome, char nomeResultado[]);
 //Transpoe uma matriz
 //ENTRADA: TM <nome> <nome-resultado sem espacos>
 //SAIDA: Imprime a matriz resultante ou ERRO (caso alguma das matrizes nao exista, suas dimensoes nao sejam as mesmas ou a matriz resultante ja existe)
 
-int SM(char nome[],char nome1[],char nomeResultado[]);
+int somarMatriz(char nome[],char nome1[],char nomeResultado[]);
 //Realiza a soma de duas matrizes
 //ENTRADA: SM <nome1> <nome2> <nome-resultado sem espacos>
 //SAIDA: mprime a matriz resultante ou ERRO (caso alguma das matrizes nao exista, suas dimensoes nao sejam as mesmas ou a matriz resultante ja existe)
 
-int DV(char nome[],char nome1[],char nomeResultado[]);
+int dividirMatriz(char nome[],char nome1[],char nomeResultado[]);
 //Divide uma matriz pela outra (elemento a elemento)
 //ENTRADA: DV <nome1> <nome2> <nome-resultado sem espacos>
 //SAIDA: OK ou ERRO (caso o vetor nao exista ou a posicao seja invalida)
 
-int MM(celula **lista,char nome[],char nome1[],char nomeResultado);
+int multiplicarMatriz(celula **lista,char nome[],char nome1[],char nomeResultado);
 
-int ME(char nome1[], char nome2[], char nomeResultado[]);
+int multiplicarElementos(char nome1[],char nome2[],char nomeResultado[]);
 
 void FE(void) 
 //finaliza o programa
