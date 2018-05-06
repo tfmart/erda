cef00d6f6fb4e53cd17a7f63dee9c565a350c9fc
tfmart <tomfmart@gmail.com>
2018-05-06 19:09:52 -0300

    Correções de erro no código

--- Projeto1/main.c
+++ Projeto1/main.c
@@ -1,5 +1,5 @@
 #include "lista.h"
-#include "matriz.h"
+#include "matrizes.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,96 +15,109 @@ typedef struct No celula;
 
 int main()
 {
-  int linhas, colunas,valor;
+  int linhas, colunas,valor, opNum;
   char nome[20], nome1[20], nomeR[20];
-  char opcao;
+  char opcao[2];
   celula *lista;
 
   do
-  { system("clear");
+  { 
+    opNum = 0;
     scanf("%s",opcao);
-    switch(opcao)
+    switch(opNum)
     {
-      case CM://CM <nome sem espaços> <número de linhas> <número de colunas>
+      if(strcmp(opcao, "CM") == 0) opNum = 1;
+      if(strcmp(opcao, "DM") == 0) opNum = 2;
+      if(strcmp(opcao, "IM") == 0) opNum = 3;
+      if(strcmp(opcao, "AE") == 0) opNum = 4;
+      if(strcmp(opcao, "AL") == 0) opNum = 5;
+      if(strcmp(opcao, "AC") == 0) opNum = 6;
+      if(strcmp(opcao, "TM") == 0) opNum = 7;
+      if(strcmp(opcao, "SM") == 0) opNum = 8;
+      if(strcmp(opcao, "DV") == 0) opNum = 9;
+      if(strcmp(opcao, "MM") == 0) opNum = 10;
+      if(strcmp(opcao, "ME") == 0) opNum = 11;
+      if(strcmp(opcao, "FE") == 0) opNum = 12;
+      
+      case 1://CM <nome sem espaços> <número de linhas> <número de colunas>
               scanf("%s",nome);
               scanf("%i %i", &linhas,&colunas);
-              if (criarMatriz(&lista, linhas, colunas, nome) != 1) printf("ERRO\n");
+              if (criarMatriz(linhas, colunas, nome) != 1) printf("ERRO\n");
               else printf("OK\n");
               break;
 
-      case DM://DM <nome>
+      case 2://DM <nome>
               scanf("%s",nome);
-              if(destruirMatriz(&lista, nome) == 1) printf("OK\n");
+              if(destruirMatriz(nome) == 1) printf("OK\n");
               else printf("ERRO\n");
               break;
 
-      case IM://IM <nome>
+      case 3://IM <nome>
               scanf("%s",nome);
-              if (imprimirMatriz(&lista, nome) == 0)
-              else printf("ERRO\n");
+              if (imprimirMatriz(nome) == 0) printf("ERRO\n");
               break;
 
-      case AE://AE <nome> <linha> <coluna> <valor>
+      case 4://AE <nome> <linha> <coluna> <valor>
               scanf("%s",nome);
               scanf("%i %i", &linhas, &colunas);
               scanf("%i",&valor);
-              if(atribuirElemento(&lista, nome,linhas,colunas,valor)==1)printf("OK\n");
+              if(atribuirElemento(nome,linhas,colunas,valor)==1)printf("OK\n");
               else printf("ERRO\n");
               break;
 
-      case AL://AL <nome> <linha> <val1>  ...  <valn> #
+      case 5://AL <nome> <linha> <val1>  ...  <valn> #
               scanf("%s", nome);
               scanf("%i", &linhas);
-              if(atribuirLinha(&lista, nome,linhas)==0)printf("OK\n");
+              if(atribuirLinha(nome,linhas)==1)printf("OK\n");
               else printf("ERRO\n");
               break;
 
-      case AC://AC <nome> <coluna> <val1>  ...  <valm> #
+      case 6://AC <nome> <coluna> <val1>  ...  <valm> #
               scanf("%s", nome);
               scanf("%i", &colunas);
-              if (atribuirColuna(&lista, nome,colunas)==0)printf("OK\n");
-              else printf("ERRO\n");
+              if (atribuirColuna(nome,colunas)==0) printf("ERRO\n");
+              else printf("OK\n");
               break;
 
-      case TM://TM <nome> <nome-resultado sem espaços>
+      case 7://TM <nome> <nome-resultado sem espaços>
               scanf("%s",nome);
               scanf("%s",nomeR);
-              if(transporMatriz(&lista, nome,nomeR)==0)
-              else printf("ERRO\n");
+              if(transporMatriz(nome,nomeR)==1) printf("ERRO\n");
               break;
 
-      case SM://SM <nome1> <nome2> <nome-resultado sem espaços >
-              scanf("%s",nome);
-              scanf("%s",nome1);
-              scanf("%s",nomeR);
-              if(somarMatriz(nome,nome1,nomeR)==0)
-              else printf("ERRO\n");
-              break;
-
-      case DV://DV <nome1> <nome2> <nome-resultado sem espaços >
+      case 8://SM <nome1> <nome2> <nome-resultado sem espaços >
               scanf("%s",nome);
               scanf("%s",nome1);
               scanf("%s",nomeR);
-              if(dividirMatriz(nome,nome1,nomeR)==0)
-              else printf("ERRO\n");
+              if(somarMatriz(nome,nome1,nomeR)==1) printf("ERRO\n");
               break;
 
-      case MM://MM <nome1> <nome2> <nome-resultado sem espaços >
+      case 9://DV <nome1> <nome2> <nome-resultado sem espaços >
               scanf("%s",nome);
               scanf("%s",nome1);
               scanf("%s",nomeR);
-              if(multiplicarMatriz(nome,nome1,nomeR)==0)
-              else printf("ERRO\n");
+              if(dividirMatriz(nome,nome1,nomeR)==0) printf("ERRO\n");
               break;
 
-      case ME://ME <nome1> <nome2> <nome-resultado sem espaços >
+      case 10://MM <nome1> <nome2> <nome-resultado sem espaços >
               scanf("%s",nome);
               scanf("%s",nome1);
               scanf("%s",nomeR);
-              if(multiplicarElementos(nome,nome1,nomeR)==0)
-              else printf("ERRO\n");
+              if(multiplicarMatriz(nome,nome1,nomeR)==1) printf("ERRO\n");
               break;
+
+      case 11://ME <nome1> <nome2> <nome-resultado sem espaços >
+              scanf("%s",nome);
+              scanf("%s",nome1);
+              scanf("%s",nomeR);
+              if(multiplicarElementos(nome,nome1,nomeR)==1) printf("ERRO\n");
+              break;
+        case 12:
+                break; 
+        default:
+                printf("OPCAO INVALIDA\n");
+                break;
     }
-  } while(opcao != FE);
+  } while(opcao != 12);
   return 0;
 }
