a4ea67cebd1dfef3f07ef969c5f647d9d7b61041
CaueBalista <37481961+CaueBalista@users.noreply.github.com>
2018-06-05 17:02:49 -0300

    Update main.c

--- Projeto 2/Rascunho/main.c
+++ Projeto 2/Rascunho/main.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "Item.h"
+#include"ST.h"
 
-
+typedef struct arvore celula;
+typedef struct tipo_item t_item;
 
 int main(int argc,char *argv[]) 
 {
@@ -12,10 +15,9 @@ int main(int argc,char *argv[])
                    
     while((scanf ("%s", palavra)) != EOF)
     {
-        resposta = insere(&lista,palavra);
+        resposta = insere(&lista, palavra);
         printf("%6d - %s\n",resposta, palavra);
     }
 
     return 0;
 }
-
