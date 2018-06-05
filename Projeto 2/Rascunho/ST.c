d0aa964a9aea9189fa4d114e9f4abfab9b9efc09
CaueBalista <37481961+CaueBalista@users.noreply.github.com>
2018-06-05 14:27:27 -0300

    Update ST.c

--- Projeto 2/Rascunho/ST.c
+++ Projeto 2/Rascunho/ST.c
@@ -1,4 +1,3 @@
-
 #include"ST.h"
 
 struct tipo_item{
@@ -6,3 +5,30 @@ int freq;
 char palavra[20];
 
 };
+
+
+int insere(celula **raiz, char palavra[]) 
+{	
+    celula *aux = (*raiz);
+    
+    if ((*raiz) == NULL)
+    {
+        (*raiz) = (celula *)malloc(sizeof(celula));
+        strcpy((*raiz)->item->palavra, palavra);
+        (*raiz)->esq = NULL;
+        (*raiz)->dir = NULL;
+        return 1;
+    }
+    else if(strcmp((*raiz)->item->palavra, palavra) == 0)
+    {
+        return (freq( &(*raiz)));
+    }
+    else if (strcmp(palavra,(*raiz)->item->palavra) > 0)
+    { 
+        return(insere(&(*raiz)->dir, palavra));
+    }
+    else if (strcmp(palavra,(*raiz)->item->palavra) < 0)
+    {
+        return (insere(&(*raiz)->esq, palavra));
+    }
+}
