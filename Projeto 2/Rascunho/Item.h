817c422a63ead654cf98b422d879b9efc7a2ae77
CaueBalista <37481961+CaueBalista@users.noreply.github.com>
2018-06-05 16:18:16 -0300

    Update Item.h

--- Projeto 2/Rascunho/Item.h
+++ Projeto 2/Rascunho/Item.h
@@ -1,3 +1,11 @@
 //CHAMADA DAS FUNÇOES
-int insere(celula **raiz, char palavra[]);
-int freq(celula **raiz);
+struct tipo_item{
+int freq;
+char palavra[20];
+
+};
+typedef struct arvore celula;
+typedef struct tipo_item t_item;
+
+void frequencia(t_item **item);
+int aloca_item(t_item **item);
