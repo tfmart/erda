d7c16f9c134025749244be67e12db82cf758f9aa
PedroZak <32416399+PedroZak@users.noreply.github.com>
2018-06-06 15:35:17 -0300

    Update Item.c

--- Projeto 2/Rascunho/Item.c
+++ Projeto 2/Rascunho/Item.c
@@ -1,23 +1,25 @@
 #include "ST.h"
 #include "Item.h"
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h> 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> 
 
 typedef struct arvore celula;
 typedef struct tipo_item t_item;
 
-void frequencia(t_item **item)
-{	
-	int aux;
-    aux = (*item)->freq;
-    aux++;
-    (*item)->freq = aux;
-    printf("%i",(*item)->freq);
+int frequencia(t_item **item)
+{
+	int frequencia = 0;
+
+    frequencia = (*item)->freq;
+    frequencia++;
+    (*item)->freq = frequencia;
+
+    return((*item)->freq + 1);
 }
 
 int aloca_item(t_item **item)
 {
-	(*item)= (t_item*)malloc(sizeof(t_item));
-	(*item)->freq=1;
+	(*item) = (t_item*)malloc(sizeof(t_item));
+	(*item)->freq = 0;
 }
