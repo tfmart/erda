0c9b277b7a41a6aa0f939aa9e551cea4cc2ffe2c
CaueBalista <37481961+CaueBalista@users.noreply.github.com>
2018-06-06 20:02:27 -0300

    Update main.c

--- Projeto 2/Rascunho/main.c
+++ Projeto 2/Rascunho/main.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -10,14 +11,48 @@ typedef struct tipo_item t_item;
 int main(int argc,char *argv[]) 
 {
     celula *lista = NULL;
-    int op, resposta;
+    int numero,op, i,j, resposta;
     char palavra[20];
-                   
+                  
+
+ 
+for(i=0;i<argc;i++){
+
+if(argv[i][0] == '-'){
+if(argv[i][1]== 'n'){ 
+
+numero = converte(argv[i]);}}
+
+}
+printf("%d",numero);
+
+
     while((scanf ("%s", palavra)) != EOF)
     {
+for(j=0;j<numero;j++){
         resposta = insere(&lista, palavra);
-        printf("%6d - %s\n",resposta, palavra);
+        printf("%6d - %s\n",resposta, palavra);}
     }
 
     return 0;
+}
+
+int converte(char caracter[]){
+
+int tamanho=0,i, X=1,numero=0;
+
+tamanho=strlen(caracter);
+
+
+
+for(i=tamanho-1;i>1;i--){
+
+
+numero+=(caracter[i]-'0')*X;
+
+X*=10;
+}
+return numero;
+
+
 }
