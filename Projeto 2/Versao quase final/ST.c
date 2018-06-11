8682ad8bc7625217613145e1cc287ed9bccdf559
CaueBalista <37481961+CaueBalista@users.noreply.github.com>
2018-06-11 15:19:17 -0300

    Update ST.c

--- Projeto 2/Versao quase final/ST.c
+++ Projeto 2/Versao quase final/ST.c
@@ -1,24 +1,27 @@
-#include "Item.h"
 #include"ST.h"
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-typedef struct arvore celula;
-typedef struct tipo_item t_item;
+void printa(celula *raiz)
+{
+	if(raiz){
+printa(raiz->esq);
+printf("%i %s\n",raiz->item->freq,raiz->item->palavra);
+if(raiz->item->prox!=NULL){
+
+printf("%i %s\n",raiz->item->prox->freq,raiz->item->palavra);
 
 
-//void printa(celula *raiz, int *print){
+}
 
-//if(raiz){
-//printa(raiz->esq,print);
-//exi
+printa(raiz->dir);
 
 
 
+}
+}
 
-
-//}
 
 void cria_arvore(celula *raiz, celula **raiz_freq){
 
@@ -28,7 +31,7 @@ void cria_arvore(celula *raiz, celula **raiz_freq){
 
 		cria_arvore(raiz->dir,raiz_freq);
 
-		nova_arvore(raiz,raiz_freq);
+		nova_arvore(raiz,&(*raiz_freq));
 
 		cria_arvore(raiz->esq,raiz_freq);
 		}
@@ -36,34 +39,72 @@ void cria_arvore(celula *raiz, celula **raiz_freq){
 }
 	
 
-int nova_arvore(celula **raiz_freq,celula *raiz ){
+int nova_arvore(celula *raiz,celula **raiz_freq ){
 
 
-if((*raiz_freq)==NULL){
+if(*raiz_freq==NULL){
 
 (*raiz_freq)=(celula*)malloc(sizeof(celula));
 
-(*raiz)->esq==NULL;
-(*raiz)->dir==NULL;
+(*raiz_freq)->esq==NULL;
+(*raiz_freq)->dir==NULL;
 
-armazena(raiz->item->palavra,raiz->item->freq,raiz_freq);
+armazena(raiz_freq,raiz->item->freq,raiz->item->palavra);
 
 }
-else if((*raiz_freq)->item->freq)==(raiz->item->palavra)){
+else if(((*raiz_freq)->item->freq)==(raiz->item->freq)){
 
-lista(raiz_freq,raiz);
+lista((*raiz_freq),&raiz);
 return 0;
 
 
 }
-else if((*raiz_freq)->item->freq)==(raiz->item->palavra)){
+else if(((*raiz_freq)->item->freq)==(raiz->item->freq)){
 return(nova_arvore(raiz,&(*raiz_freq)->esq));
 return(nova_arvore(raiz,&(*raiz_freq)->dir));
 
 
 }
 }
-int insere(celula **raiz, char palavra[]) 
+
+int armazena(celula **raiz,int freq,char palavra[]){
+
+
+(*raiz)->item=(t_item*)malloc(sizeof(t_item));
+
+strcpy((*raiz)->item->palavra,palavra);
+(*raiz)->item->freq=freq;
+(*raiz)->item->prox=NULL;
+return 0;}
+
+void lista(celula *raiz, celula **raiz_freq){
+
+ t_item *aux=(t_item*)malloc(sizeof(t_item));
+
+ t_item *auxiliador=(t_item*)malloc(sizeof(t_item));
+
+auxiliador->prox=NULL;
+
+aux=(*raiz_freq)->item;
+
+strcpy(auxiliador->palavra,(raiz)->item->palavra);
+
+if(aux == NULL){
+
+(*raiz_freq)->item=auxiliador;
+
+}
+
+else{
+
+while(aux->prox!=NULL){
+aux=aux->prox;
+aux->prox=auxiliador;}
+
+}
+}
+
+void insere(celula **raiz, char palavra[]) 
 {	
     celula *aux = (*raiz);
     
@@ -76,19 +117,19 @@ int insere(celula **raiz, char palavra[])
         strcpy((*raiz)->item->palavra, palavra);
         (*raiz)->esq = NULL;
         (*raiz)->dir = NULL;
-        return 1;
+        return ;
+
     }
     else if(strcmp((*raiz)->item->palavra, palavra) == 0)
     {
-		 (frequencia( &(*raiz)->item));
+		 frequencia(&(*raiz)->item);
     }
     else if (strcmp(palavra,(*raiz)->item->palavra) > 0)
     { 
-        return(insere(&(*raiz)->esq, palavra));
+        insere(&(*raiz)->esq, palavra);
     }
     else if (strcmp(palavra,(*raiz)->item->palavra) < 0)
     {
-        return (insere(&(*raiz)->dir, palavra));
+        insere(&(*raiz)->dir, palavra);
     }
 }
-
