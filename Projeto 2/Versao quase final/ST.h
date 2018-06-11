442dffd555373875c50b65e984be8ca4231473ed
CaueBalista <37481961+CaueBalista@users.noreply.github.com>
2018-06-11 16:10:38 -0300

    Update ST.h

--- Projeto 2/Versao quase final/ST.h
+++ Projeto 2/Versao quase final/ST.h
@@ -9,6 +9,6 @@ typedef struct arvore celula;
 void insere(celula **raiz, char palavra[]);
 void cria_arvore(celula *raiz, celula **raiz_freq);
 int nova_arvore(celula *raiz,celula **raiz_freq );
-void printa(celula *raiz);
+void printa(celula *raiz,int *numero);
 int armazena(celula **raiz,int freq,char palavra[]);
 void lista(celula *raiz, celula **raiz_freq);
