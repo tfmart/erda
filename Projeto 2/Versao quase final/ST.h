f798c70bbe8ed90aaae34d9e80d37f79893092e3
CaueBalista <37481961+CaueBalista@users.noreply.github.com>
2018-06-10 21:47:59 -0300

    Create ST.h

---
+++ Projeto 2/Versao quase final/ST.h
@@ -0,0 +1,12 @@
+typedef struct tipo_item t_item;
+
+struct arvore{
+    struct arvore *dir, *esq;
+    t_item *item;
+};
+
+typedef struct arvore celula;
+
+int insere(celula **raiz, char palavra[]);
+void cria_arvore(celula *raiz, celula **raiz_freq);
+int nova_arvore(celula **raiz_freq,celula *raiz );
