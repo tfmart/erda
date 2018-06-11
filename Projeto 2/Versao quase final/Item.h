5699865c6928e2bc75aba78d1b3f55cf52c0a73b
CaueBalista <37481961+CaueBalista@users.noreply.github.com>
2018-06-10 21:46:39 -0300

    Create Item.h

---
+++ Projeto 2/Versao quase final/Item.h
@@ -0,0 +1,12 @@
+struct tipo_item{
+	int freq;
+	char palavra[20];
+struct tipo_item *prox;
+};
+
+typedef struct arvore celula;
+typedef struct tipo_item t_item;
+
+int frequencia(t_item **item);
+int aloca_item(t_item **item);
+void lista(celula *raiz, celula **raiz_freq);
